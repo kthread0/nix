e028eb8ad075338244e4278c28984b4884f210f5
kthread0 <dev@kthread.dev>
2025-12-12 20:19:28 -0300

    move to zen

--- hosts/deterministic/boot.nix
+++ hosts/deterministic/boot.nix
@@ -65,7 +65,60 @@
 		};
 	};
 
-	boot.kernelPackages = pkgs.linuxPackages_latest;
+	boot.kernelPackages = pkgs.linuxKernel.kernels.linux_zen;
 
 	boot.initrd.luks.devices."luks-fbe22c00-3e4b-4204-9b78-f1ff3bdd7b79".device = "/dev/disk/by-uuid/fbe22c00-3e4b-4204-9b78-f1ff3bdd7b79";
+
+	boot.kernelParams = [
+		"accept_memory=eager"
+		"acpi=force"
+		"acpi_os_name=Linux"
+		"acpi_osi=Linux"
+		"pcie_aspm=force"
+		"iomem=strict"
+		"iommu=force"
+		"iommu.strict=1"
+		"iommu.forcedac=1"
+		"io_delay=none"
+		"ro"
+		"rcutree.kthread_prio=99"
+		"realloc"
+		"pci=pcie_bus_perf"
+		"workqueue.power_efficient=0"
+		"amd_pstate=active"
+		"amd_iommu=pgtbl_v2"
+		"gbpages"
+		"threadirqs"
+		"preempt=full"
+		"lsm=landlock,lockdown,yama,integrity,bpf"
+		"slab_nomerge"
+		"init_on_alloc=1"
+		"randomize_kstack_offset=on"
+		"init_on_free=1"
+		"page_alloc.shuffle=1"
+		"plymouth.use-simpledrm"
+		"rng_core.default_quality=1000"
+		"pti=on"
+		"vsyscall=none"
+		"mce=1"
+		"audit=1"
+		"audit_backlog_limit=8192"
+		"loglevel=0"
+		"quiet"
+		"splash"
+		"rd.luks.allow-discards"
+		"clocksource.verify_n_cpus=12"
+		"tpm_tis.force=1"
+		"tpm_crb.force=1"
+		"tpm_tis.interrupts=1"
+		"reboot=acpi"
+		"transparent_hugepage=always"
+		"transparent_hugepage_shmem=always"
+		"transparent_hugepage_tmpfs=always"
+		"hugepage_alloc_threads=12"
+		"hugepagesz=2M"
+		"hugepages=16"
+		"hugepagesz=1G"
+		"hugepages=4"
+	];
 }
